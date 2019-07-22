b03dd240fcec44909d363656849f21fd058a5832
Driek <driek.vhees@gengo.com>
2019-07-22 23:37:39 +0900

    Some simple formatting

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import TodoDetails from './TodoDetails';
 
-const TodoList = ({ todos, toggleTodo }) => {
-  return (
-    <div className="todo-list">
-      <ul>
-        { todos.map(todo => <TodoDetails
-            key={todo.id}
-            onClick={() => {toggleTodo(todo.id)}} todo={todo} />)
-          }
-      </ul>
-    </div>
-  );
+const TodoList = ({ todos, toggleTodo }) => (
+  <div className="todo-list">
+    <ul>
+      {todos.map(todo => (
+        <TodoDetails key={todo.id} todo={todo} onClick={() => toggleTodo(todo.id)} />
+        ))}
+    </ul>
+  </div>
+)
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired
 }
  
 export default TodoList;
